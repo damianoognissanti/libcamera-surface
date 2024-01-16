7268e5ed4389d8b2390321dd4f47da200bd75fde
damianoognissanti <97738787+damianoognissanti@users.noreply.github.com>
2024-01-16 21:20:11 +0100

    Update libcamera.nix 0.0.3 -> 0.2.0
    - Updated libcamera to 0.2.0
    - Used gg=G to format file

--- libcamera.nix
+++ libcamera.nix
@@ -1,25 +1,23 @@
-# This config is for setting up the computer and installing GNOME.
-# All other packages are installed via ./users/damiano.nix
-{ config, pkgs, ... }: 
+{ config, pkgs, ... }:
 {
   environment.systemPackages =
-  let
-    libcam_recent = with pkgs; stdenv.mkDerivation rec {
-      name = "libcamera-0.0.3";
-      version = "0.0.3";
-      src = fetchgit {
-        url = "https://git.libcamera.org/libcamera/libcamera.git";
-        rev = "v${version}";
-        hash = "sha256-0/wvH07bJRKFwYnOARRJNzH8enIX3TNnWQnJdfpfvgE=";
-      };
-      
-    postPatch = ''
-      patchShebangs utils/
-    '';
+    let
+      libcam_recent = with pkgs; stdenv.mkDerivation rec {
+        name = "libcamera-0.2.0";
+        version = "0.2.0";
+        src = fetchgit {
+          url = "https://git.libcamera.org/libcamera/libcamera.git";
+          rev = "v${version}";
+          hash = "sha256-x0Im9m9MoACJhQKorMI34YQ+/bd62NdAPc2nWwaJAvM=";
+        };
 
-    strictDeps = true;
+        postPatch = ''
+          patchShebangs utils/
+        '';
 
-    buildInputs = [
+        strictDeps = true;
+
+        buildInputs = [
       # IPA and signing
       openssl
 
@@ -39,42 +37,41 @@
 
       # yamlparser
       libyaml
-  
+
       gtest
-  ];
+    ];
 
-  nativeBuildInputs = [
-    meson
-    ninja
-    pkg-config
-    python3
-    python3Packages.jinja2
-    python3Packages.pyyaml
-    python3Packages.ply
-    python3Packages.sphinx
-    graphviz
-    doxygen
-    openssl
-  ];
+    nativeBuildInputs = [
+      meson
+      ninja
+      pkg-config
+      python3
+      python3Packages.jinja2
+      python3Packages.pyyaml
+      python3Packages.ply
+      python3Packages.sphinx
+      graphviz
+      doxygen
+      openssl
+    ];
 
-  mesonFlags = [
-    "-Dpipelines=uvcvideo,vimc,ipu3"
-    "-Dipas=vimc,ipu3"
-   # "-Dprefix=/usr"
-    "-Dgstreamer=enabled"
-#    "-Dv4l2=true"
-    "-Dqcam=disabled"
-#    "-Dlc-compliance=disabled" # tries unconditionally to download gtest when enabled
-    # Avoid blanket -Werror to evade build failures on less
-    # tested compilers.
-#    "-Dwerror=false"
+    mesonFlags = [
+      "-Dpipelines=uvcvideo,vimc,ipu3"
+      "-Dipas=vimc,ipu3"
+      "-Dgstreamer=enabled"
+      "-Dv4l2=true"
+      "-Dqcam=disabled"
+      "-Dlc-compliance=disabled" # tries unconditionally to download gtest when enabled
+      # Avoid blanket -Werror to evade build failures on less
+      # tested compilers.
+      "-Dwerror=false"
     ];
 
   # Fixes error on a deprecated declaration
   NIX_CFLAGS_COMPILE = "-Wno-error=deprecated-declarations";
 
   # Silence fontconfig warnings about missing config
-  FONTCONFIG_FILE = makeFontsConf { fontDirectories = []; };
+  #FONTCONFIG_FILE = makeFontsConf { fontDirectories = []; };
 
   # libcamera signs the IPA module libraries at install time, but they are then
   # modified by stripping and RPATH fixup. Therefore, we need to generate the
@@ -87,8 +84,8 @@
     ../src/ipa/ipa-sign-install.sh src/ipa-priv-key.pem $out/lib/libcamera/ipa_*.so
   '';
 
-    };
+};
   in
   [ libcam_recent ];
-
 }
+
